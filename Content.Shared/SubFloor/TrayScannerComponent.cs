9f866a3dfcbb98a9a2dc934a41a90dd63d508a14
adamsogm <adamsong@users.noreply.github.com>
2026-06-26 14:50:36 -0400

    Remove duplicate using directive

--- Content.Shared/SubFloor/TrayScannerComponent.cs
+++ Content.Shared/SubFloor/TrayScannerComponent.cs
@@ -1,5 +1,4 @@
 using Robust.Shared.Audio;
-using Robust.Shared.Audio; // Funky change
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes; // Funky change
 using Robust.Shared.Serialization;
